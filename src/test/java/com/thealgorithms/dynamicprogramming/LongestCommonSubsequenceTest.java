609d9c2b6d53302f0a188af08a43f8291ebde29c
tjex <tillmanjex@mailbox.org>
2023-02-16 16:35:33 +0100

    feat : LCS test creation wip : issue #9

--- src/test/java/com/thealgorithms/dynamicprogramming/LongestCommonSubsequenceTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/LongestCommonSubsequenceTest.java
@@ -14,16 +14,17 @@ public class LongestCommonSubsequenceTest {
         String actualLCS = LongestCommonSubsequence.getLCS(str1, str2);
         assertEquals(expectedLCS, actualLCS);
     }
-    //
-    // @Test
-    // void lcsString(){
-    //
-    // }
-    //
-    // @Test
-    // void main(){
-    //
-    // }
+
+    @Test
+    void lcsString(){
+        String str1 = "ABCD";
+        String str2 = "ABDC";
+        String expectedString = "ABD";
+        int[][] actualString = LongestCommonSubsequence.lcsString(str1, str2, );
+        assertEquals(expectedString, actualString);
+
+    }
+
 
     @AfterAll
     static void printCoverage(){
