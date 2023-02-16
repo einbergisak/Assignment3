ad54e6e2f72c0c4286a4c1446145372920331c94
tjex <tillmanjex@mailbox.org>
2023-02-16 14:16:52 +0100

    feat : added LCS test file and added missing extension to LVP test file

---
+++ src/test/java/com/thealgorithms/dynamicprogramming/LongestCommonSubsequenceTest.java
@@ -0,0 +1,23 @@
+package com.thealgorithms.dynamicprogramming;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LongestCommonSubsequenceTest {
+
+    @Test
+    void getLCS(){
+
+    }
+
+    @Test
+    void lcsString(){
+
+    }
+
+    @Test
+    void main(){
+
+    }
+
+}
