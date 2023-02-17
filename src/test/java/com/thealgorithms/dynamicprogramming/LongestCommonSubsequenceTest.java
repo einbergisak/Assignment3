7478f13f8776f5ee87a06d3b29e5edb839aeff68
tjex <tillmanjex@mailbox.org>
2023-02-17 15:42:51 +0100

    fix : removed unit tests to keep branch clean for later merging : issue #6

--- src/test/java/com/thealgorithms/dynamicprogramming/LongestCommonSubsequenceTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/LongestCommonSubsequenceTest.java
@@ -6,26 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LongestCommonSubsequenceTest {
 
-    @Test
-    void getLCS(){
-        String str1 = "ABCD";
-        String str2 = "ABDC";
-        String expectedLCS = "ABD";
-        String actualLCS = LongestCommonSubsequence.getLCS(str1, str2);
-        assertEquals(expectedLCS, actualLCS);
-    }
-
-    // @Test
-    // void lcsString(){
-    //     String str1 = "ABCD";
-    //     String str2 = "ABDC";
-    //     String expectedString = "ABD";
-    //     int[][] actualString = LongestCommonSubsequence.lcsString(str1, str2, );
-    //     assertEquals(expectedString, actualString);
-    //
-    // }
-
-
+    // tests to be implemented in order to properly 
+    // run the printCoverage function.
+    
     @AfterAll
     static void printCoverage(){
         System.out.println(LongestCommonSubsequence.branchesReached);
