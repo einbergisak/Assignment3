3b8f5ae34fc7c562942e41a78663a11dda760a3f
tjex <tillmanjex@mailbox.org>
2023-02-18 05:56:23 +0100

    feat : getLCS test created : issue #9

--- src/test/java/com/thealgorithms/dynamicprogramming/LongestCommonSubsequenceTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/LongestCommonSubsequenceTest.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LongestCommonSubsequenceTest {
 
-    // tests to be implemented in order to properly 
-    // run the printCoverage function.
+    @Test
+    void getLCS(){
+        String str1 = "ABCD";
+        String str2 = "ABDC";
+        String expectedLCS = "ABD";
+        String actualLCS = LongestCommonSubsequence.getLCS(str1, str2);
+        assertEquals(expectedLCS, actualLCS);
+    }
     
     @AfterAll
     static void printCoverage(){
