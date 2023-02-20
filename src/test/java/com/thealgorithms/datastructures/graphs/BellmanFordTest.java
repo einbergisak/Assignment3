499f01ddfc931bcefd11b70cbd6044e0de529d5a
Adam Ezzaim <adam.ezzaim@epfl.ch>
2023-02-20 13:42:29 +0100

    feat(BellmanFord): add manual coverage for BellmanFord::go #5

---
+++ src/test/java/com/thealgorithms/datastructures/graphs/BellmanFordTest.java
@@ -0,0 +1,18 @@
+package com.thealgorithms.datastructures.graphs;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BellmanFordTest {
+
+    @Test
+    void go() {
+    }
+
+    @AfterAll
+    static void printCoverage(){
+        System.out.println(BellmanFord.branchesReached);
+    }
+}
