093a0610ec9831a5807437c8b5f02fab0c5a992a
Gabrielstaifo <gabrielstaifo@gmail.com>
2023-02-20 15:44:50 +0100

    refactor, documentations for the tests and the flags have been added, issue #21

--- src/main/java/com/thealgorithms/strings/ValidParentheses.java
+++ src/main/java/com/thealgorithms/strings/ValidParentheses.java
@@ -6,25 +6,53 @@ package com.thealgorithms.strings;
 //        Every close bracket has a corresponding open bracket of the same type.
 
 
+import java.util.*;
+
 public class ValidParentheses {
+	public static HashSet<String> branchesReached = new HashSet<>();
 	public static boolean isValid(String s) {
 		char[] stack = new char[s.length()];
 		int head = 0;
 		for(char c : s.toCharArray()) {
+			branchesReached.add("1.1");
 			switch(c) {
 				case '{':
+					// Requirement: the input is "{".
+					branchesReached.add("1.2");
 				case '[':
+					// Requirement: the input is "[".
+					branchesReached.add("1.3");
 				case '(':
+					// Requirement: the input is "(".
+					branchesReached.add("1.4");
 					stack[head++] = c;
 					break;
 				case '}':
-					if(head == 0 || stack[--head] != '{') return false;
+					// Requirement: the input is "}".
+					branchesReached.add("1.5.1");
+					if(head == 0 || stack[--head] != '{') {
+						// Requirement: head is 0 OR stack[--head] is not '{'.
+						branchesReached.add("1.5.2");
+						return false;
+					}
 					break;
 				case ')':
-					if(head == 0 || stack[--head] != '(') return false;
+					// Requirement: the input is ")".
+					branchesReached.add("1.6.1");
+					if(head == 0 || stack[--head] != '(') {
+						// Requirement: head is 0 OR stack[--head] is not '('.
+						branchesReached.add("1.6.2");
+						return false;
+					}
 					break;
 				case ']':
-					if(head == 0 || stack[--head] != '[') return false;
+					// Requirement: the input is "]".
+					branchesReached.add("1.7.1");
+					if(head == 0 || stack[--head] != '[') {
+						// Requirement: head is 0 OR stack[--head] is not '['.
+						branchesReached.add("1.7.2");
+						return false;
+					}
 					break;
 			}
 		}
